0df37013bc0be687a8865eea7a8b489d3c224fb1
reshma1249 <res_ma91@rediffmail.com>
2016-06-01 12:00:59 -0500

    Add files via upload

---
+++ assignment1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace assignment1
+{
+    partial class movies
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listChoices = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemClose});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // menuItemClose
+            // 
+            this.menuItemClose.Name = "menuItemClose";
+            this.menuItemClose.Size = new System.Drawing.Size(97, 22);
+            this.menuItemClose.Text = "Quit";
+            this.menuItemClose.Click += new System.EventHandler(this.menuItemClose_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemAbout});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // menuItemAbout
+            // 
+            this.menuItemAbout.Name = "menuItemAbout";
+            this.menuItemAbout.Size = new System.Drawing.Size(111, 22);
+            this.menuItemAbout.Text = "Author";
+            this.menuItemAbout.Click += new System.EventHandler(this.menuItemAbout_Click);
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Location = new System.Drawing.Point(132, 96);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(165, 20);
+            this.txtEntry.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(409, 96);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(82, 204);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(63, 17);
+            this.rb1.TabIndex = 4;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Comedy";
+            this.rb1.UseVisualStyleBackColor = true;
+            this.rb1.CheckedChanged += new System.EventHandler(this.rb1_CheckedChanged);
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(259, 204);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(54, 17);
+            this.rb2.TabIndex = 5;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Horror";
+            this.rb2.UseVisualStyleBackColor = true;
+            this.rb2.CheckedChanged += new System.EventHandler(this.rb2_CheckedChanged);
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(438, 204);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(71, 17);
+            this.rb3.TabIndex = 6;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "Romance";
+            this.rb3.UseVisualStyleBackColor = true;
+            this.rb3.CheckedChanged += new System.EventHandler(this.rb3_CheckedChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(272, 440);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listChoices
+            // 
+            this.listChoices.Location = new System.Drawing.Point(82, 242);
+            this.listChoices.Multiline = true;
+            this.listChoices.Name = "listChoices";
+            this.listChoices.Size = new System.Drawing.Size(431, 176);
+            this.listChoices.TabIndex = 8;
+            // 
+            // movies
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 492);
+            this.Controls.Add(this.listChoices);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.rb3);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "movies";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Movies";
+            this.Load += new System.EventHandler(this.movies_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuItemClose;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuItemAbout;
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox listChoices;
+    }
+}
+
---
+++ assignment1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment1
+{
+    public partial class movies : Form
+    {
+        List<String> comedyList;
+        List<String> horrorList;
+        List<String> romanceList;
+        public movies()
+        {
+            InitializeComponent();
+        }
+
+        private void movies_Load(object sender, EventArgs e)
+        {
+            comedyList = new List<string>();
+            horrorList = new List<string>();
+            romanceList = new List<string>();
+
+        }
+
+        private void menuItemClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuItemAbout_Click(object sender, EventArgs e)
+        {
+            frmAuthor authorForm = new frmAuthor();
+            authorForm.ShowDialog();
+        }
+
+     
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string text = txtEntry.Text;
+            if (text.Length > 0)
+            {
+                if (rb1.Checked)
+                {
+                    comedyList.Add(text);
+                    updateDisplay(comedyList);
+                }
+                else if (rb2.Checked)
+                {
+                    horrorList.Add(text);
+                    updateDisplay(horrorList);
+                }
+                else if (rb3.Checked)
+                {
+                    romanceList.Add(text);
+                    updateDisplay(romanceList);
+                }
+                else
+                {
+                    MessageBox.Show("Please select a category");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid input!!!");
+            }
+
+        }
+        private void updateDisplay(List<String> list)
+        {
+            listChoices.Text = "";
+            foreach (string item in list)
+            {
+                
+                listChoices.AppendText(item);
+                listChoices.AppendText(Environment.NewLine);
+            }
+        }
+
+        private void rb1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb1.Checked)
+                updateDisplay(comedyList);
+        }
+
+        private void rb2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb2.Checked)
+                updateDisplay(horrorList);
+        }
+
+        private void rb3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb3.Checked)
+                updateDisplay(romanceList);
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (rb1.Checked)
+            {
+                comedyList.Sort();
+                updateDisplay(comedyList);
+            }
+            else if (rb2.Checked)
+            {
+                horrorList.Sort();
+                updateDisplay(horrorList);
+            }
+            else if (rb3.Checked)
+            {
+                romanceList.Sort();
+                updateDisplay(romanceList);
+            }
+            else
+            {
+                MessageBox.Show("Please select a category");
+            }
+        }
+
+     
+
+        
+    }
+}
